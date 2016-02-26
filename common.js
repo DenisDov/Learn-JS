bffbb8036fa9eed7b00bf162191951b734bb0d69
DenisDov <xsyndromex@gmail.com>
2016-02-26 09:47:05 +0200

    Added Eloquent function

--- common.js
+++ common.js
@@ -428,3 +428,14 @@ function checkObj(checkProp) {
 }
 
 checkObj("gift");
+
+//==========================Eloquent JS======== Functions======================
+
+var power = function(base, exponent) {
+  var result = 1;
+  for (var count = 0; count < exponent; count++)
+    result *= base;
+  return result;
+};
+
+console.log(power(2, 10));

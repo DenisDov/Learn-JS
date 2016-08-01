a183a55cbfba8da8325ff62c3059e8aeef2c287a
DenisDov <xsyndromex@gmail.com>
2016-08-01 11:14:45 +0300

    comm

--- common.js
+++ common.js
@@ -907,3 +907,29 @@ printPersonName(bob);
 // then use printPersonName to print your name
 var me = new Person("Denis", 32);
 printPersonName(me);
+
+
+// DRY
+
+// the original Animal class and sayName method
+function Animal(name, numLegs) {
+    this.name = name;
+    this.numLegs = numLegs;
+}
+Animal.prototype.sayName = function() {
+    console.log("Hi my name is " + this.name);
+};
+
+// define a Penguin class
+function Penguin(name) {
+    this.name = name;
+    this.numLegs = 2;
+}
+
+
+// set its prototype to be a new instance of Animal
+Penguin.prototype = new Animal();
+
+var penguin = new Penguin("Bob");
+
+penguin.sayName();
